a05f5ef0fca301b3617ee448897e983b024696c4
Jonas Barsten <jonasbarsten@gmail.com>
2020-02-17 06:20:36 +0100

    GUI enhancements

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import io from 'socket.io-client';
 import SocketIOFileUpload from 'socketio-file-upload';
 import { Input, Button, Row, Col, Container, Table, Progress, Badge } from 'reactstrap';
-import { FiPlay, FiTrash } from 'react-icons/fi';
+import { FiPlay, FiTrash, FiStopCircle } from 'react-icons/fi';
 import { FaStop } from 'react-icons/fa';
+import { Levels } from 'react-activity';
+// import Countdown from 'react-countdown';
+
+import 'react-activity/dist/react-activity.css'
 
 const socket = io('torgetserver.kristofferlo.no');
+
+const countdownRenderer = ({ hours, minutes, seconds }) => {
+	return <span>{hours}:{minutes}:{seconds}</span>;
+};
+
+function msToTime(s) {
+  // Pad to 2 or 3 digits, default is 2
+  function pad(n, z) {
+    z = z || 2;
+    return ('00' + n).slice(-z);
+  }
+
+  var ms = s % 1000;
+  s = (s - ms) / 1000;
+  var secs = s % 60;
+  s = (s - secs) / 60;
+  var mins = s % 60;
+  var hrs = (s - mins) / 60;
+
+  return pad(mins) + ':' + pad(secs);
+}
 
 export default class Dashboard extends Component {
 
@@ -39,12 +64,19 @@ export default class Dashboard extends Component {
   }
 
   playFile(fileName) {
-  	console.log('Playplay ' + fileName);
   	socket.emit('playFile', fileName);
   }
 
+  stopFile(fileName) {
+  	socket.emit('stopFile', fileName);
+  }
+
+  stopRandomFile() {
+  	socket.emit('stopRandomFile');
+  }
+
   playRandomComposition() {
-  	socket.emit('playRandomComposition');
+  	socket.emit('playRandomComposition', 20*60*1000);
   }
 
 	render () {
@@ -58,6 +90,10 @@ export default class Dashboard extends Component {
 		const diskAvaliable = (socketData && socketData.diskUsage) ? socketData.diskUsage.available : 0;
 		const diskTotal = (socketData && socketData.diskUsage) ? socketData.diskUsage.total : 0;
 		const diskUsage = ((diskTotal - diskAvaliable) / diskTotal) * 100;
+		const currentlyPlaying = (socketData && socketData.currentlyPlaying) ? socketData.currentlyPlaying : [];
+		const stopRandomFileButton = (currentlyPlaying.length < 2) ? null : <Button style={{marginRight: '5px'}} color="info" onClick={this.stopRandomFile}><FiStopCircle /> Stop random</Button>;
+		const countDown = (socketData && socketData.stopwatch) ? <p>Composition duration: {msToTime(socketData.stopwatch)}</p> : null;
+		const compositionButton = countDown ? null : <Button color="primary" onClick={this.playRandomComposition}>Play random 20 min composition</Button>;
 		let ips = (socketData && socketData.network) ? socketData.network : [];
 		const indexOfVPNIp = ips.indexOf('10.14.1.2');
 		if (indexOfVPNIp > -1) {
@@ -98,23 +134,35 @@ export default class Dashboard extends Component {
 								  <tr>
 								    <th>Name</th>
 								    <th>Controls</th>
+								    <th>Activity</th>
 								  </tr>
 								</thead>
 								<tbody>
 									{files.map((file, i) => {
+
+										const active = (currentlyPlaying.indexOf(file) === -1) ? null : <span><Levels /></span>;
+
 										return (
 											<tr key={i}>
 												<td>{file}</td>
 												<td>
-													<Button color="primary" style={{marginRight: '5px'}} onClick={() => this.playFile(file)}><FiPlay /></Button>
-													<Button color="danger" onClick={() => this.deleteFile(file)}><FiTrash /></Button>
+													<Button color="success" style={{marginRight: '5px'}} onClick={() => this.playFile(file)}><FiPlay /></Button>
+													<Button color="primary" style={{marginRight: '5px'}} onClick={() => this.stopFile(file)}><FaStop /></Button>
+													<Button color="danger" style={{marginRight: '5px'}} onClick={() => this.deleteFile(file)}><FiTrash /></Button>
+												</td>
+												<td>
+													{active}
 												</td>
 											</tr>
 										);
 									})}
 									<tr>
 										<td>
-											<Button color="primary" onClick={this.playRandomComposition}>Play random 20 min composition</Button>
+											{compositionButton}
+											{countDown}
+										</td>
+										<td>
+											{stopRandomFileButton}
 										</td>
 										<td>
 											<Button color="danger" onClick={this.stopAll}><FaStop /> Stop all</Button>
